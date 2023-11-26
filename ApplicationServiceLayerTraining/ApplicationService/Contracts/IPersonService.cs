2814fef6e0c8b55fc69a1cbb45c1807ceb73ffee
Shbz <Shahbazi.amh@gmail.com>
2023-11-27 00:51:09 +0330

    Add Application Service Layer/Dtos created/Service contracts and frameworks create/Service classes created but not implemented.

---
+++ ApplicationServiceLayerTraining/ApplicationService/Contracts/IPersonService.cs
@@ -0,0 +1,9 @@
+﻿using ApplicationServiceLayerTraining.ApplicationService.Contracts.ServiceFrameworks;
+using ApplicationServiceLayerTraining.ApplicationService.Dtos.PersonDtos;
+using ApplicationServiceLayerTraining.Models.DomainModels.PersonAggregates;
+
+namespace ApplicationServiceLayerTraining.ApplicationService.Contracts;
+
+public interface IPersonService : IService<Person,ServiceCreatePersonDto,ServiceSelectPersonDto,ServiceUpdatePersonDto,ServiceDeletePersonDto>
+{
+}
